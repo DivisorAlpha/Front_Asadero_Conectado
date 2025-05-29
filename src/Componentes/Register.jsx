3eb6190b18735a7e3dc2c1821493aec42e331aaf
chebaztian <u20222208924@usco.edu.co>
2025-05-29 11:28:08 -0500

    Validacion de formulario Register.jsx y render de nav.jsx

--- src/Componentes/Register.jsx
+++ src/Componentes/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/Formulario.css';
 import axios from 'axios';
@@ -11,38 +11,138 @@ const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [mostrarContrasena, setMostrarContrasena] = useState(false);
+
+  // INICIO CAMBIOS CHEBAS
+  // Estados para errores de validación 
+
+  const [nombreError, setNombreError] = useState('');
+  const [celularError, setCelularError] = useState('');
+  const [cedulaError, setCedulaError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [formValid, setFormValid] = useState(false);
+
+  //Expresiones regulares 
+
+  const regexNombre = /^[a-zA-Z\s]{3,40}$/;
+  const regexCelular = /^\d{10}$/;
+  const regexCedula = /^\d{6,10}$/;
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+
+  //Funciones para validar cada campo
+
+  const validarNombre = (valor) => {
+   setNombre(valor); 
+   if (!regexNombre.test(valor)) {
+    setNombreError('El nombre debe tener solo letras y al menos 3 caracteres');
+  } else {
+    setNombreError('');
+  }
+  };
+
+  const validarCelular = (valor) => {
+    setCelular(valor);
+    if(!regexCelular.test(valor)) {
+      setCelularError('El número de celular debe tener 10 dígitos');
+    } else {
+      setCelularError('');
+    }
+  }
+
+  const validarCedula = (valor) => {
+    setCedula(valor);
+    if(!regexCedula.test(valor)) {
+      setCedulaError('La cédula debe tener entre 6 y 10 dígitos');
+    } else {
+      setCedulaError('');
+    }
+  }
+
+  const validarEmail = (valor) => {
+    setEmail(valor);
+    if(!regexEmail.test(valor)) {
+      setEmailError('El correo electrónico no es válido');
+    } else {
+      setEmailError('');
+    }
+  }
+
+  const validarPassword = (valor) => {
+    setPassword(valor);
+    if(!regexPassword.test(valor)) {
+      setPasswordError('La contraseña debe tener al menos 6 caracteres, incluyendo letras y números');
+    } else {
+      setPasswordError('');
+    }
+  }
+
+  useEffect(() => {
+  if (!nombreError && !celularError && !cedulaError && !emailError && !passwordError) {
+      setFormValid(true);
+  } else {
+    setFormValid(false);
+  }
+}, [nombre, celular, cedula, email, password, nombreError, celularError, cedulaError, emailError, passwordError]);
+
+  //Tambien se modifica cada onChange de los inputs para llamar a las funciones de validación,
+  //cambiando setNombre por validarNombre, etc.
+  //Así como el agregar despues del final de cada input 
+  //{nombreError && <p className="formulario-error">{nombreError}</p>} para que muestre el error debajo del input correspondiente.
+  //El handleRegister ahora con el if valida con validForm.
+  //se modifica el console.log.
+
+  // FIN CAMBIOS CHEBAS
+
   const navigate = useNavigate();
 
-  const handleRegister = (e) => {
-    e.preventDefault();
+  const handleRegister = async (e) => {
+  e.preventDefault();
 
-    if (nombre && celular && cedula && email && password) {
-      console.log('Registro exitoso');
-      navigate('/Login'); // Redirige al login después de registrarse
-    } else {
-      alert('Por favor, completa todos los campos');
+  if (formValid) {
+    try {
+      const response = await axios.post('http://localhost:8000/api/nuevoCliente', {
+        "nombre": nombre,
+        "numeroCelular": celular,
+        "cedula": cedula,
+        "correoElectronico": email,
+        "contrasena": password
+      });
+
+      console.log('Usuario registrado:', response.data);
+      alert('Registro exitoso');
+      navigate('/Login');
+    } catch (error) {
+      console.error('Error al registrar usuario:', error.response?.data || error.message);
+      alert('Ocurrió un error al registrar el usuario');
     }
-  };
+  } else {
+    alert('Por favor, corrija los errores en el formulario.');
+    console.log('Error en el registro');
+  }
+};
+
 
   const toggleContrasena = () => {
     setMostrarContrasena(!mostrarContrasena);
   };
 
-  const registrarUsuario = async () => {
-  try {
-    const response = await axios.post('http://localhost:8000/api/nuevoCliente', {
-  "nombre": nombre,
-  "numeroCelular": celular,
-  "cedula": cedula,
-  "correoElectronico": email,
-  "contrasena": password
-});
+  //Ahora en handlerRegister se va a encargar de hacer esto.
+//   const registrarUsuario = async () => {
+//   try {
+//     const response = await axios.post('http://localhost:8000/api/nuevoCliente', {
+//   "nombre": nombre,
+//   "numeroCelular": celular,
+//   "cedula": cedula,
+//   "correoElectronico": email,
+//   "contrasena": password
+// });
 
-    console.log('Usuario registrado:', response.data);
-  } catch (error) {
-    console.error('Error al registrar usuario:', error.response?.data || error.message);
-  }
-};
+//     console.log('Usuario registrado:', response.data);
+//   } catch (error) {
+//     console.error('Error al registrar usuario:', error.response?.data || error.message);
+//   }
+// };
 
   return (
     <>
@@ -65,33 +165,37 @@ const Register = () => {
             className="formulario-registro-input"
             placeholder="Nombre completo"
             value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
+            onChange={(e) => validarNombre(e.target.value)}
             required
           />
+          {nombreError && <p className="formulario-error">{nombreError}</p>}
           <input
             type="tel"
             className="formulario-registro-input"
             placeholder="Número de celular"
             value={celular}
-            onChange={(e) => setCelular(e.target.value)}
+            onChange={(e) => validarCelular(e.target.value)}
             required
           />
+          {celularError && <p className="formulario-error">{celularError}</p>}
           <input
             type="text"
             className="formulario-registro-input"
             placeholder="Cédula"
             value={cedula}
-            onChange={(e) => setCedula(e.target.value)}
+            onChange={(e) => validarCedula(e.target.value)}
             required
           />
+          {cedulaError && <p className="formulario-error">{cedulaError}</p>}
           <input
             type="email"
             className="formulario-registro-input"
             placeholder="Correo Electrónico"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => validarEmail(e.target.value)}
             required
           />
+          {emailError && <p className="formulario-error">{emailError}</p>}
 
           {/* Contraseña con toggle */}
           <div className="formulario-registro-password-container">
@@ -100,7 +204,7 @@ const Register = () => {
               className="formulario-registro-input"
               placeholder="Contraseña"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => validarPassword(e.target.value)}
               required
             />
             <button
@@ -110,9 +214,11 @@ const Register = () => {
             >
               {mostrarContrasena ? 'Ocultar' : 'Mostrar'}
             </button>
+            
           </div>
+          {passwordError && <p className="formulario-error">{passwordError}</p>}
 
-          <button type="submit" className="formulario-registro-boton" onClick={registrarUsuario}>Registrarse</button>
+          <button type="submit" className="formulario-registro-boton" disabled={!formValid}>Registrarse</button>
         </form>
       </div>
     </>
